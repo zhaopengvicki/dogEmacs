3960cea86365d8a8ff063afdec445e3aa6eb6c55
DogLooksGood <doglooksgood@hotmail.com>
2016-10-25 01:09:03 +0800

    rewrite

---
+++ init.el
@@ -0,0 +1,36 @@
+;; Minimal setup for Clojure development & Org document.
+
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(setq package-archives
+      '(("gnu-cn" . "http://elpa.zilongshanren.com/gnu/")
+        ("melpa-cn" . "http://elpa.zilongshanren.com/melpa/")
+        ("melpa-stable-cn" . "	http://elpa.zilongshanren.com/melpa-stable/")
+        ("marmalade-cn" . "http://elpa.zilongshanren.com/marmalade/")
+        ("org-cn" . "http://elpa.zilongshanren.com/org/")))
+(package-initialize)
+
+;; -----------------------------------------------------------------------------
+;; Use Package
+;; -----------------------------------------------------------------------------
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t
+      use-package-always-defer t)
+(require 'org)
+(require 'ob-tangle)
+(require 'use-package)
+
+;; -----------------------------------------------------------------------------
+;; Basics
+;; -----------------------------------------------------------------------------
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+(when (file-exists-p "~/.emacs.d/PRIVATE.org")
+  (org-babel-load-file "~/.emacs.d/PRIVATE.org"))
+(server-start)
+
