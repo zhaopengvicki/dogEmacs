bd45031b4de917ecb3edbf85820a258a5a7b24bd
DogLooksGood <doglooksgood@hotmail.com>
2016-10-28 16:52:15 +0800

    update

--- init.el
+++ init.el
@@ -34,3 +34,19 @@
   (org-babel-load-file "~/.emacs.d/PRIVATE.org"))
 (server-start)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
+ '(package-selected-packages
+   (quote
+    (powerline parinfer zenburn-theme use-package spacemacs-theme solarized-theme rainbow-delimiters org-plus-contrib org-bullets magit highlight-symbol expand-region exec-path-from-shell emmet-mode doom-themes counsel-projectile company clj-refactor circe arjen-grey-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
