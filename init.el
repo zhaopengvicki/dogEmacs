a92fc5241af8c8bc1eda038e404bc4fcc43dd237
doglooksgood <doglooksgood@hotmail.com>
2017-07-18 11:41:54 +0800

    主题

--- init.el
+++ init.el
@@ -35,3 +35,17 @@
 (cl-letf (((symbol-function 'message) #'format))
   (org-babel-load-file (expand-file-name "~/.emacs.d/README.org")))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (spaceline-all-the-icons spaceline monroe zenburn-theme use-package ujelly-theme spacemacs-theme solarized-theme smart-mode-line sayid rainbow-delimiters parinfer org-plus-contrib org-bullets neotree magit leuven-theme hl-sexp highlight-symbol github-theme github-modern-theme git-gutter-fringe git-draft flycheck-tip flycheck-pos-tip flycheck-clojure expand-region exec-path-from-shell dracula-theme doom-themes darktooth-theme darcula-theme counsel-projectile company clj-refactor cider-eval-sexp-fu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
