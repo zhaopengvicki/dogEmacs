aaab161494364550d637bb11d1e44406c29e4233
doglooksgood <doglooksgood@hotmail.com>
2017-12-18 18:43:20 +0800

    UPDATE

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 (setq package-archives
       '(("gnu-cn" . "http://elpa.zilongshanren.com/gnu/")
         ("melpa-cn" . "http://elpa.zilongshanren.com/melpa/")
-        ("melpa-stable-cn" . "	http://elpa.zilongshanren.com/melpa-stable/")
+        ("melpa-stable-cn" . "http://elpa.zilongshanren.com/melpa-stable/")
         ("marmalade-cn" . "http://elpa.zilongshanren.com/marmalade/")
         ("org-cn" . "http://elpa.zilongshanren.com/org/")))
 (package-initialize)
@@ -32,5 +32,12 @@
 (use-package cl)
 (require 'cl)
 
-(cl-letf (((symbol-function 'message) #'format))
-  (org-babel-load-file (expand-file-name "~/.emacs.d/README.org")))
+
+;; If README.el exists, load README.el directly.
+;; Otherwise, use org-babel-load-file.
+(if (file-exists-p "~/.emacs.d/README.el")
+    (load-file "~/.emacs.d/README.el")
+  (cl-letf (((symbol-function 'message) #'format))
+    (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))))
+
+;; END Here
